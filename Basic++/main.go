3f72343014d3a26706d2e4953101fcf8327d8b70
Trym Erlend Staurheim <trymes13%student.uia.no>
2020-02-11 08:27:50 +0100

    added betterSort and evenBetterSort

--- Basic++/main.go
+++ Basic++/main.go
@@ -11,7 +11,10 @@ func main() {
 	//enTilHundre(1, 300)
 	//favFrukt()
 	//whiletelling()
-	bubbleSort()
+
+	//bubbleSort()
+	betterSort()
+	evenBetterSort()
 }
 
 //Oppgave 1 Lag en funksjon som printer “Hello” 100 ganger.  Hint: FOR
@@ -79,22 +82,24 @@ func whiletelling() {
 //
 //}
 func bubbleSort() {
-	var num = 7
-	a := []int{0, 0, 0, 0, 0, 0, 0, 0,0}
+	var a [10]int
 	fmt.Print(a, "initial array: \n ")
-	for i := 0; i < num; i++ { //adds 5 values to array
+	for i := 0; i < len(a); i++ { //adds 5 values to array
 		a[i] = rand.Intn(7)
 		fmt.Println(a)
 	}
 	fmt.Println("Starting sort")
 	var temp = 0
 	var temp1 = 0;
-	for i := 0; i < len(a); i++ {
+	for i := 0; i < (len(a) * 2); i++ {
+		if (i == len(a)) {
+			i = 0
+		}
 		if (a[i] >= a[i+1]) {
 			swap(temp1, temp, a, i)
-		} else{
+		} else {
 			temp1, temp := a[i], a[i+1]
-			fmt.Println(temp1,temp)
+			fmt.Println(temp1, temp)
 			a[i] = temp
 			a[i+1] = temp1
 			fmt.Println(a)
@@ -103,10 +108,54 @@ func bubbleSort() {
 	}
 }
 
-func swap(temp1 int, temp int, a []int, i int) {
+func swap(temp1 int, temp int, a [10]int, i int) {
 	temp1, temp = a[i+1], a[i]
 	fmt.Println(temp1, temp)
 	a[i] = temp1
 	a[i+1] = temp
 	fmt.Println(a)
+}
+func betterSort() {
+	list := createlist();
+	fmt.Print(list, "initial array: \n ")
+	fmt.Println("startin sort")
+	len := len(list)
+	for i := 0; i < len; i++ {
+		for j := 0; j < len-1; j++ {
+			if list[j] > list[j+1] {
+				temp := list[j+1]
+				list[j+1] = list[j]
+				list[j] = temp
+				fmt.Println(list)
+			}
+		}
+	}
+}
+
+func createlist() []int {
+	var list =[]int {0,0,0,0,0,0,0,0,0,0}
+	for i := 0; i < len(list); i++ { //adds 5 values to array
+		list[i] = rand.Intn(100)
+		fmt.Println(list)
+
+	}
+	return list
+}
+
+func evenBetterSort() {
+	list := createlist()
+	fmt.Println(list)
+	n := len(list)
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 1; i < n; i++ {
+			if list[i-1] > list[i] {
+				fmt.Println("swapping", list)
+				list[i-1], list[i] = list[i], list[i-1]
+				swapped = true
+			}
+		}
+	}
+	fmt.Println(list)
 }
